78ec60ada1a0cb48df26e509cdd7cdd57900300e
machufay <mattyangfei@gmail.com>
2025-06-11 15:35:25 -0500

    s

---
+++ test.js
@@ -0,0 +1,32 @@
+// Declare variables below to save the different divs of your story.
+let apple = document.querySelector(".apple");
+let body = document.querySelector("body");
+
+apple.addEventListener('mouseover', function() {
+    body.style.backgroundColor = "red"
+
+});
+
+
+
+
+
+
+
+
+// When you're ready to make event handlers, uncomment the code below. 
+//  - Then fill in the blanks with the correct variables.
+
+
+// INSERT_VARIABLE.addEventListener('click', function(){
+
+// });
+
+// INSERT_VARIABLE.addEventListener('click', function(){
+
+// });
+
+
+// INSERT_VARIABLE.addEventListener('click', function(){
+
+// });
