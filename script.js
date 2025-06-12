21fe675f0875a817694898b4272436cb550031f2
machufay <mattyangfei@gmail.com>
2025-06-12 12:58:19 -0500

    f

--- script.js
+++ script.js
@@ -5,15 +5,24 @@ let optionTwo = document.querySelector(".option-two");
 let bottle1 = document.querySelector(".bottle1");
 let drinkbottle2 = document.querySelector(".drinkbottle2");
 let gunneddown2 = document.querySelector(".gunneddown2");
-let gunneddown3 = document.querySelector(".runover3");
+let gunneddown3 = document.querySelector(".gunneddown3");
 let slip3 = document.querySelector(".slip3");
 let yell4 = document.querySelector(".yell4");
 let look4 = document.querySelector(".look4");
 let locked5 = document.querySelector(".locked5");
+let walkaway5 = document.querySelector(".walkaway5");
 
 bottle1.style.display = "block";
 
 optionOne.addEventListener("click", function() {
+
+    if (situation=="look4") {
+        situation = "locked5";
+        look4.style.display = "none";
+        locked5.style.display = "block";
+        optionOne.innerHTML = "huh";
+        optionTwo.innerHTML = "to be continued";
+    }
     if (situation=="slip3") {
         situation = "look4";
         slip3.style.display = "none";
@@ -35,8 +44,19 @@ optionOne.addEventListener("click", function() {
         optionOne.innerHTML = "drink?";
         optionTwo.innerHTML = "put it back down";
     }
-    
-    
+    if (situation=="restart") {
+        situation = "bottle";
+        bottle1.style.display = "block";
+        drinkbottle2.style.display = "none";
+        gunneddown2.style.display = "none";
+        gunneddown3.style.display = "none";
+        slip3.style.display = "none";
+        yell4.style.display = "none";
+        look4.style.display = "none";
+        locked5.style.display = "none";
+        optionOne.innerHTML = "Pick it up";
+        optionTwo.innerHTML = "leave it";
+    }
     
 });
 
@@ -46,43 +66,30 @@ optionTwo.addEventListener("click", function() {
         bottle1.style.display = "none";
         gunneddown2.style.display = "block";
         optionOne.innerHTML = "restart";
-        optionTwo.style.display = "none";
+        optionTwo.innerHTML = "good job";    
     }
     if (situation=="drinkbottle2") {
         situation = "restart";
         drinkbottle2.style.display = "none";
         gunneddown3.style.display = "block";
         optionOne.innerHTML = "restart";
-        optionTwo.style.display = "none";
+        optionTwo.innerHTML = "good job";    
     }
     if (situation=="slip3") {
         situation = "restart";
         slip3.style.display = "none";
         yell4.style.display = "block";
         optionOne.innerHTML = "restart";
-        optionTwo.style.display = "none";
+        optionTwo.innerHTML = "good job";    
+    }
+    if (situation=="look4") {
+        situation = "restart";
+        yell4.style.display = "none";
+        walkaway5.style.display = "block";
+        optionOne.innerHTML = "restart";
+        optionTwo.innerHTML = "good job";
     }
     
 });
 
 
-
-
-
-
-// When you're ready to make event handlers, uncomment the code below. 
-//  - Then fill in the blanks with the correct variables.
-
-
-// INSERT_VARIABLE.addEventListener('click', function(){
-
-// });
-
-// INSERT_VARIABLE.addEventListener('click', function(){
-
-// });
-
-
-// INSERT_VARIABLE.addEventListener('click', function(){
-
-// });
